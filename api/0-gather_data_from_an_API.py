992c4c78ef4bdf881c7503d9ecb5c3996caa838f
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-20 12:04:31 -0700

    0: i really feel like this should work

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,27 +7,34 @@ returns information about his/her TODO list progress
 import requests
 
 
-def gather(empl_id):
+def display_employee_progress(employee_id):
     """ script must display to stdout the employee TODO list progress """
     url = "https://jsonplaceholder.typicode.com/users"
-    empl_url = f"{url}/{empl_id}"
-    todo_url = f"{empl_url}/todos"
+    empl_url = f"{url}/{employee_id}"
+    todo_url = f"{url}/todos"
 
     empl_response = requests.get(empl_url)
-    todo_response = requests.get(todo_url)
     empl_data = empl_response.json()
+    todo_response = requests.get(todo_url,
+                                 params={"userId": employee_id})
     todo_data = todo_response.json()
 
-    name = empl_data.get('name')
-    num_done = sum(1 for t in todo_data if t['completed'] is True)
+    empl_name = empl_data["name"]
+    completed_tasks = [t["title"] for t in todo_data if t["completed"]]
+    num_done = len(completed_tasks)
+    num_total = len(todo_data)
 
-    print(f"Employee {name} is done with \
-          tasks({len(todo_data)}/{num_done}):\n")
-    for t in todo_data:
-        if t['completed'] is True:
-            print(f"\t {t['title']}")
+    print(f"Employee {empl_name} is done with \
+          tasks({num_done}/{num_total}):")
+    for t in completed_tasks:
+        print(f"\t{t}")
 
 if __name__ == "__main__":
     import sys
 
-    gather(int(sys.argv[1]))
+    if len(sys.argv) == 2:
+        try:
+            display_employee_progress(int(sys.argv[1]))
+        except requests.exceptions.RequestException:
+            print("Well.. that didn't work \
+                  (as far as I'm aware, this shouldn't ever print)")

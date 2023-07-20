1df0b764e612aaa98a7bc3804245d50736595458
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-20 13:22:26 -0700

    0: are you ready to work now?..

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-write a Python script that, using the given REST API,
-for a given employee ID,
-returns information about his/her TODO list progress
+write Python script that, using the given REST API,
+for a given employee id,
+returns information about their todo list progress
 """
 import requests
 
 
 def display_employee_progress(employee_id):
-    """ script must display to stdout the employee TODO list progress """
+    """ script must display to stdout the employee todo list progress """
     url = "https://jsonplaceholder.typicode.com"
     empl_url = f"{url}/user/{employee_id}"
     todo_url = f"{url}/todos"
 
     empl_data = requests.get(empl_url).json()
     todo_data = requests.get(todo_url,
-                                 params={"userId": employee_id}).json()
+                             params={"userId": employee_id}).json()
 
     empl_name = empl_data.get("name")
     completed_tasks = [t["title"] for t in todo_data if t["completed"]]

18e73ff1dd9804eb1835fc66b939bff998686a10
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-20 13:46:15 -0700

    0: it should not be possible for this not to pass at this point

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,10 +4,10 @@ import requests
 import sys
 
 
-def display_employee_progress():
+def gather_api_data():
     """ gather and print api data """
     if(len(sys.argv) != 2):
-        print("Error: not 3 arguments")
+        print("Error not 3 commands")
 
     employee_id = sys.argv[1]
 
@@ -16,9 +16,16 @@ def display_employee_progress():
     todo_data = requests.get('https://jsonplaceholder.typicode.com/todos',
                              params={"userId": employee_id}).json()
     EMPLOYEE_NAME = user_data.get("name")
-    completed_tasks = [todo.get("title") for todo in todo_data if todo["completed"]]
-    NUMBER_OF_DONE_TASKS = len(completed_tasks)
-    TOTAL_NUMBER_OF_TASKS = len(todo_data)
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+
+    completed_tasks = []
+
+    for item in todo_data:
+        TOTAL_NUMBER_OF_TASKS += 1
+        if item.get("completed"):
+            NUMBER_OF_DONE_TASKS += 1
+            completed_tasks.append(item.get("title"))
 
     print('Employee {} is done with tasks({}/{}):'
           .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
@@ -27,4 +34,4 @@ def display_employee_progress():
 
 
 if __name__ == "__main__":
-    display_employee_progress()
+    gather_api_data()
